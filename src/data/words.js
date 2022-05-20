65876a28fd3b19a40ee5fac93fc32c15130db3fd
carolinavaleriano <102779256+carolinavaleriano@users.noreply.github.com>
2022-05-20 17:40:39 -0300

    React Project Complete

---
+++ src/data/words.js
@@ -0,0 +1,8 @@
+export const wordsList = {
+    carro: ["Motor", "Porta", "Capô", "Pneu", "Antena", "Banco", "Volante", "Cinto", "Freio", "Acelerador", "Retrovisor", "Seta", "Farol"],
+    fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja", "Goiaba", "Pêssego", "Mexerica", "Jambo", "Pitaya", "Abacate", "Abacaxi", "Acerola"],
+    corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos", "Ombro", "Cabeça", "Dedos", "Joelho", "Barriga", "Calcanhar", "Unha", "Nariz", "Tornozelo"],
+    computador: ["Mouse", "Teclado", "Monitor", "Gabinete", "Processador", "CPU", "Microprocessador", "RAM", "Barramento", "Slots"],
+    programação: ["Linguagem", "Framework", "JavaScript", "React", "API", "Algoritmo", "Bug", "Chatbot", "Codar", "Debug", "Software"],
+    alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo", "Atum", "Alface", "Beterraba", "Danone", "Espaguete", "Farofa", "Iogurte", "Macarrão"],
+};
